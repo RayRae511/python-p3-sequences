51c13a396dfadcac2774629e391e52ebd13c618d
Raymond Oeta <oetaray@gmail.com>
2023-08-20 13:58:39 +0300

    completed work

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    fibb_list = []
+    r, a = 0, 1
+    for o in range(length):
+        fibb_list.append(r)
+        r, a=a, r+a
+    print(fibb_list)
     pass
